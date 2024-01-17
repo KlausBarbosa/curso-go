84e204d74b740126c8691b9fa4185348db64cb4f
KlausBarbosa <klauas.boliveira@gmail.com>
2024-01-17 07:53:29 -0300

    add swagger doc with OpenAPI

--- curso-go/APIs/cmd/server/main.go
+++ curso-go/APIs/cmd/server/main.go
@@ -1,17 +1,40 @@
 package main
 
 import (
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/jwtauth"
 	"github.com/klausbarbosa/curso-go/curso-go/APIs/configs"
+	_ "github.com/klausbarbosa/curso-go/curso-go/APIs/docs"
 	"github.com/klausbarbosa/curso-go/curso-go/APIs/internal/entity"
 	"github.com/klausbarbosa/curso-go/curso-go/APIs/internal/infra/database"
 	"github.com/klausbarbosa/curso-go/curso-go/APIs/internal/infra/webserver/handlers"
+	httpSwagger "github.com/swaggo/http-swagger"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
+	"log"
 	"net/http"
 )
 
+// @title           Go Expert API Example
+// @version         1.0
+// @description     Product API with auhtentication
+// @termsOfService  http://swagger.io/terms/
+
+// @contact.name   Klaus Barbosa
+// @contact.url    https://github.com/KlausBarbosa/KlausBarbosa
+// @contact.email  https://www.linkedin.com/in/klaus-barbosa-707b8a185/
+
+// @license.name   Full Cycle License Course
+// @license.url    https://github.com/KlausBarbosa/KlausBarbosa
+
+// @host      localhost:8000
+// @BasePath  /
+// @securityDefinitions.apikey ApiKeyAuth
+// @in header
+// @name Authorization
 func main() {
-	_, err := configs.LoadConfig("./cmd/server")
+	configs, err := configs.LoadConfig("./cmd/server")
 	if err != nil {
 		panic(err)
 	}
@@ -23,6 +46,38 @@ func main() {
 	productDB := database.NewProduct(db)
 	productHandler := handlers.NewProductHandler(productDB)
 
-	http.HandleFunc("/products", productHandler.CreateProduct)
-	http.ListenAndServe(":8000", nil)
+	userDB := database.NewUser(db)
+	userHandler := handlers.NewUserHandler(userDB)
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger) //intercepta as requisicoes e injeta Logs
+	r.Use(middleware.Recoverer)
+
+	r.Use(middleware.WithValue("jwt", configs.TokenAuth))
+	r.Use(middleware.WithValue("JwtExpiresIn", configs.JwtExpiresIn))
+
+	//Agrupamento de rotas
+	r.Route("/products", func(r chi.Router) {
+		r.Use(jwtauth.Verifier(configs.TokenAuth))
+		r.Use(jwtauth.Authenticator)
+		r.Post("/", productHandler.CreateProduct)
+		r.Get("/", productHandler.GetProducts)
+		r.Get("/{id}", productHandler.GetProduct)
+		r.Put("/{id}", productHandler.UpdateProduct)
+		r.Delete("/{id}", productHandler.DeleteProduct)
+	})
+
+	r.Post("/users", userHandler.Create)
+	r.Post("/users/generate_token", userHandler.GetJWT)
+
+	r.Get("/docs/*", httpSwagger.Handler(httpSwagger.URL("http://localhost:8000/docs/doc.json")))
+
+	http.ListenAndServe(":8000", r)
+}
+
+func LogRequest(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("Request: %s %s", r.Method, r.URL.Path)
+		next.ServeHTTP(w, r)
+	})
 }
