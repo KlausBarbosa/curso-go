4d9b3211a257e1fa9dfa943dfa7e91b3c378267f
KlausBarbosa <klauas.boliveira@gmail.com>
2024-02-07 22:28:16 -0300

    add events module add event dispatchers tests

--- curso-go/context/2-Context-ClientServer/server/main.go
+++ curso-go/context/2-Context-ClientServer/server/main.go
@@ -16,7 +16,7 @@ func main() {
 func handler(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 	log.Println("Request iniciada")
-	defer log.Println("Request finalizada")
+	defer log.Println("Request finaliza da")
 	select {
 	case <-time.After(5 * time.Second):
 		log.Println("Request processada com sucesso")     //imprime no servidor stdout
